e4d43014de4fade64865346bb77e8d4b3157a043
sabi <codespectator@gmail.com>
2021-06-23 23:49:16 +0900

    M: add custom dataloader to customdataset

--- configs/solov2/solov2_r50_fpn_8gpu_1x.py
+++ configs/solov2/solov2_r50_fpn_8gpu_1x.py
@@ -17,7 +17,7 @@ model = dict(
         num_outs=5),
     bbox_head=dict(
         type='SOLOv2Head',
-        num_classes=81,
+        num_classes=2,
         in_channels=256,
         stacked_convs=4,
         seg_feat_channels=512,
@@ -56,25 +56,23 @@ test_cfg = dict(
     sigma=2.0,
     max_per_img=100)
 # dataset settings
-dataset_type = 'CocoDataset'
-data_root = 'data/coco/'
+dataset_type = 'Hdf5Dataset'
+data_root = '/workspace/datablock/dataset_cv2/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
-    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(type='Resize',img_scale=(512, 512), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+    dict(type='Collect', keys=['img', 'gt_masks']),
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1333, 800),
+        img_scale=(512, 512),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
@@ -90,19 +88,15 @@ data = dict(
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_train2017.json',
-        img_prefix=data_root + 'train2017/',
+        ann_file=data_root + 'trainSetInfo.txt',
+        img_prefix=data_root + 'train/',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
+        ann_file=data_root + 'trainSetInfo.txt',
+        img_prefix=data_root + 'train/',
         pipeline=test_pipeline),
-    test=dict(
-        type=dataset_type,
-        ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
-        pipeline=test_pipeline))
+  )
 # optimizer
 optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
@@ -124,10 +118,10 @@ log_config = dict(
 # yapf:enable
 # runtime settings
 total_epochs = 12
-device_ids = range(8)
+device_ids = range(1)
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/solov2_release_r50_fpn_8gpu_1x'
+work_dir = './work_dirs/solov2_release_r50_fpn_1gpu_1x'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
--- mmdet/datasets/custom.py
+++ mmdet/datasets/custom.py
@@ -150,3 +150,171 @@ class CustomDataset(Dataset):
             results['proposals'] = self.proposals[idx]
         self.pre_pipeline(results)
         return self.pipeline(results)
+
+
+
+import h5py
+import torch
+from torch.utils import data
+import numpy as np
+from pycocotools import mask as m
+from pathlib import Path
+import cv2
+import ast
+import os
+@DATASETS.register_module
+class Hdf5Dataset(Dataset):
+    CLASSES = None
+    def __init__(self, ann_file, pipeline, data_root=None, img_prefix='', test_mode=False,):
+        super().__init__()
+        self.ann_file = ann_file
+        self.data_root = data_root
+        self.img_prefix = img_prefix
+        self.pipeline = pipeline
+        self.test_mode = test_mode
+        self.baseDir = Path(img_prefix)
+        self.chunkSize = 2000 
+        assert self.baseDir.is_dir()
+        self.files = sorted(self.baseDir.glob('*.h5'), key=lambda x: int(x.parts[-1][-6:-3]))
+        self.img_infos={'witdh':512, 'height':512}
+        with open(ann_file, 'r') as f:
+            self.totalData = int(f.read())
+        self.data_info=[]
+        # processing pipeline
+        self.pipeline = Compose(pipeline)
+        self._set_group_flag()
+
+
+    def _set_group_flag(self):
+        """Set flag according to image aspect ratio.
+
+        Images with aspect ratio greater than 1 will be set as group 1,
+        otherwise group 0.
+        """
+        self.flag = np.zeros(len(self), dtype=np.uint8)
+        for i in range(len(self)):
+            self.flag[i] = 0
+
+    # def pre_pipeline(self, results):
+    #     results['seg_fields'] =    []
+    #     results['img'] = img
+    #     results['img_shape'] = img.shape
+    #     results['ori_shape'] = img.shape
+    #     #results['gt_bboxes'] = ann_info['bboxes']
+    #     #results['bbox_fields'].append('gt_bboxes')
+    #     results['gt_semantic_seg'] = mask
+    #     r#esults['seg_fields'].append('gt_semantic_seg')
+    #     #results['img_prefix'] = self.img_prefix
+    #     #results['seg_prefix'] = self.seg_prefix
+    #     #results['proposal_file'] = self.proposal_file
+    #     #results['bbox_fields'] = []
+    #     #results['mask_fields'] = []
+    #     #results['seg_fields'] = []
+
+    # - "img_shape": shape of the image input to the network as a tuple
+    #     (h, w, c).  Note that images may be zero padded on the bottom/right
+    #     if the batch tensor is larger than this shape.
+
+    # - "scale_factor": a float indicating the preprocessing scale
+
+    # - "flip": a boolean indicating if image flip transform was used
+
+    # - "filename": path to the image file
+
+    # - "ori_shape": original shape of the image as a tuple (h, w, c)
+
+    # - "pad_shape": image shape after padding
+
+    # - "img_norm_cfg": a dict of normalization information:
+    #     - mean - per channel mean subtraction
+    #     - std - per channel std divisor
+    #     - to_rgb - bool indicating if bgr was converted to rgb
+
+    def __len__(self):
+        
+        return self.totalData
+
+
+    def __getitem__(self, idx):
+        """
+        dataset.attrs['polygon1'] is hair mask
+        dataset.attrs['polygon2'] is face mask
+        """
+        if self.test_mode:
+            return self.prepare_test_img(idx)
+        while True:
+            data = self.prepare_train_img(idx)
+            #self.overlayMask(data)
+            if data is None:
+                idx = self._rand_another(idx)
+                continue
+            return data
+
+    def prepare_train_img(self, idx):
+        fidx = idx//self.chunkSize
+        with h5py.File(self.files[fidx]) as f:
+            dataset = f['group{0:03}'.format(fidx)]['{}'.format(idx)]
+            img = cv2.imdecode(np.array(dataset), flags=cv2.IMREAD_COLOR)
+            results ={
+                'img': img,
+                'img_shape': img.shape,
+                'ori_shape': img.shape,
+                'filename': os.path.join(self.files[fidx],'group{0:03}'.format(fidx),'{}'.format(idx))
+                # 'gt_bboxes' ,
+                #  'gt_labels'
+                
+            }
+            for k in ['polygon1', 'polygon2']:
+                if k in dataset.attrs.keys():
+                    results['gt_masks']=m.decode(ast.literal_eval(dataset.attrs[k]))
+            self.overlayMask(results)
+                    
+        return self.pipeline(results)
+
+    # def prepare_test_img(self, idx):
+    #     img_info = self.img_infos[idx]
+    #     results = dict(img_info=img_info)
+    #     if self.proposals is not None:
+    #         results['proposals'] = self.proposals[idx]
+    #     self.pre_pipeline(results)
+    #     return self.pipeline(results)
+
+
+    # def __getitem__(self, item):
+    #     """
+    #     dataset.attrs['polygon1'] is hair mask
+    #     dataset.attrs['polygon2'] is face mask
+    #     """
+    #     fidx = item//self.chunkSize
+    #     with h5py.File(self.files[fidx]) as f:
+    #         dataset = f['group{0:03}'.format(fidx)]['{}'.format(item)]
+    #         data ={
+    #             'image':  cv2.imdecode(np.array(dataset), flags=cv2.IMREAD_COLOR)
+    #         }
+    #         for k in ['polygon1', 'polygon2']:
+    #             if k in dataset.attrs.keys():
+    #                 data[k]=m.decode(ast.literal_eval(dataset.attrs[k]))
+
+    #         # attributes string values
+    #         #for k, val in dataset.attrs.items():
+    #         #    data[k] = val
+
+    #         #display mask overlay image
+    #         #self.overlayMask(data)
+    #     return data
+
+    def overlayMask(self, data):
+        img, hair_mask  = data['img'], data['gt_masks']
+        img[:,:,1][hair_mask!=0]=255
+        #cv2.imwrite('example.jpg',img)
+        cv2.imwrite('img.jpg',img)
+        # cv2.waitKey(0)
+        
+
+
+if __name__ == "__main__":
+    root = '/data/hairData' #this path needs to be modified
+    dataset = Hdf5Dataset(root)
+    loader = data.DataLoader(dataset, batch_size=4, shuffle=True, num_workers=2)
+    for batch in loader:
+        print(batch['image'].shape)
